8df251e2a042cecd007c332f9a466d5d0b89383b
sbrosnah <77689031+sbrosnah@users.noreply.github.com>
2024-11-11 21:50:13 -0600

    changed

--- ml-visualizations/next.config.ts
+++ ml-visualizations/next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/machine-learning-visualizations",
   output: 'export',
-  reactStrictMode: true
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
