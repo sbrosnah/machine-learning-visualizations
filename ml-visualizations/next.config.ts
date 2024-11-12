0540e2d7523431cd293962d174dddb9cf6b92c7c
sbrosnah <77689031+sbrosnah@users.noreply.github.com>
2024-11-11 18:00:44 -0600

    actions setup

--- ml-visualizations/next.config.ts
+++ ml-visualizations/next.config.ts
@@ -1,19 +1,16 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === 'production';
-const repositoryName = '/machine-learning-visualizations';
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = '/machine-learning-visualizations';
 
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isProduction ? repositoryName : '',
-  assetPrefix: isProduction ? repositoryName : '',
+  basePath: isProd ? repoName : '',
+  assetPrefix: isProd ? repoName : '',
   trailingSlash: true,
-  publicRuntimeConfig: {
-    basePath: isProduction ? repositoryName : '',
-  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
     return config;
