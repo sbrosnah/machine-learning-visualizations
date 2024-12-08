e466ae71c01d1ccd587780b4cb9db1486fd0a732
Spencer Brosnahan <77689031+sbrosnah@users.noreply.github.com>
2024-12-08 00:45:07 -0600

    fixed sizing

--- src/app/(concepts)/mle/page.tsx
+++ src/app/(concepts)/mle/page.tsx
@@ -1,6 +1,5 @@
 import MarkdownPage from "@/app/components/MarkdownPage";
 import CoinFlip from "@/app/components/mle/CoinFlip";
-// import MLEVisualization from "@/app/components/mle/MLEVisualization";
 import { MARKDOWN_DELIMITER } from "@/lib/constants";
 import { getMarkdownContent } from '@/lib/markdown-utils';
 import Image from 'next/image'
@@ -16,13 +15,13 @@ export default async function MLEPage() {
             <MarkdownPage content={chunks[0]}/>
             <CoinFlip/>
             <MarkdownPage content={chunks[1]}/> 
-            <Image className="max-w-5xl mx-auto px-4 py-8" src="/images/log-odds.png" alt="MLE Diagram" width={1000} height={1000}/>
+            <Image className="max-w-5xl mx-auto px-4 py-8 w-full h-auto" src="/images/log-odds.png" alt="MLE Diagram" width={1000} height={1000}/>
             <MarkdownPage content={chunks[2]}/> 
-            <Image className="max-w-5xl mx-auto px-4 py-8" src="/images/normal-pdf.png" alt="Gaussian Plot" width={700} height={500}/>
+            <Image className="max-w-5xl mx-auto px-4 py-8 w-full h-auto" src="/images/normal-pdf.png" alt="Gaussian Plot" width={700} height={500}/>
             <MarkdownPage content={chunks[3]}/> 
-            <Image className="max-w-5xl mx-auto px-4 py-8" src="/images/jensen's-inequality.png" alt="Gaussian Plot" width={700} height={500}/>
+            <Image className="max-w-5xl mx-auto px-4 py-8 w-full h-auto" src="/images/jensen's-inequality.png" alt="Gaussian Plot" width={700} height={500}/>
             <MarkdownPage content={chunks[4]}/> 
-            <Image className="max-w-5xl mx-auto px-4 py-8" src="/images/negated-log.png" alt="Gaussian Plot" width={700} height={500}/>
+            <Image className="max-w-5xl mx-auto px-4 py-8 w-full h-auto" src="/images/negated-log.png" alt="Gaussian Plot" width={700} height={500}/>
             <MarkdownPage content={chunks[5]}/> 
         </>
     );
