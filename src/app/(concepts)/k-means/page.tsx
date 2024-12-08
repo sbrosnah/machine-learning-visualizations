402ff7530041316293d42b27a5eb85d482d89f1c
sbrosnah <77689031+sbrosnah@users.noreply.github.com>
2024-12-07 23:59:12 -0600

    Delete page.tsx

--- src/app/(concepts)/k-means/page.tsx
+++
@@ -1,9 +0,0 @@
-import MarkdownPage from "@/app/components/MarkdownPage";
-import { getMarkdownContent } from "@/lib/markdown-utils";
-
-export default async function KMeansPage() {
-    const content = await getMarkdownContent('k-means')
-    return (
-        <MarkdownPage content={content}/>
-    );
-}
